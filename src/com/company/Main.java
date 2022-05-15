a2aeaa65e99ee1fd7c269f29cf29eccb4d10cb57
MrASyntex <continentaldispatch@gmail.com>
2022-05-15 11:19:36 -0700

    Solar System Basic Code

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+SolarSystem FactorX = new SolarSystem();
+
+FactorX.Sun="1";
+FactorX.Moon="4";
+FactorX.Planets="2";
+FactorX.Stars="1000";
+
+
+    }
+}
---
+++ src/com/company/SolarSystem.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class SolarSystem {
+    String Sun;
+    String Moon;
+    String Planets;
+    String Stars;
+
+}
