7ac53b16da425fa2bcc5b43e03d1df070817cda4
MrASyntex <continentaldispatch@gmail.com>
2022-05-15 11:35:45 -0700

    The feature sun been added succesfuly

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,13 @@ FactorX.Sun="1";
 FactorX.Moon="4";
 FactorX.Planets="2";
 FactorX.Stars="1000";
+ //adding features ot our sun
 
+        FeatureSun Xfact = new FeatureSun();
+        Xfact.color="Green";
+        Xfact.radius="12312313";
+        Xfact.heat=" 752257F";
+        
 
     }
 }
